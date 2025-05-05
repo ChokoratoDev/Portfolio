7e7197c43e35a64d42eba6de2b2d92c96b6b5f5b
ChokoDev <jphilblanchet@yahoo.fr>
2025-05-05 12:31:54 +0200

    Day ? : Bandeau rework, Form front, responsive home

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.querySelector(".burger");
-    const menu = document.querySelector(".menu");
+	const burger = document.querySelector(".burger");
+	const menu = document.querySelector(".menu");
+	const form = document.querySelector("form");
+	const formActionButton = document.querySelector("button.lets-talk");
 
+	burger.addEventListener("click", () => {
+		burger.classList.toggle("active");
+		menu.classList.toggle("active");
+	});
 
-    burger.addEventListener("click", () => {
-        burger.classList.toggle("active");
-        menu.classList.toggle("active");
-    })
-})
+	form.style.display = "none";
+
+	formActionButton.addEventListener("click", () => {
+		if (form.classList.contains("active")) {
+			form.classList.toggle("active");
+			setTimeout(() => {
+				form.style.display = "none";
+			}, 400);
+		} else {
+			form.style.display = "block";
+			form.offsetHeight;
+			form.classList.add("active");
+		}
+	});
+});
